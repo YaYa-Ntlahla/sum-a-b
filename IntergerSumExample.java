39cebb79acaf8692ccdb1e75c260e170229478a9
Yandisa Ntlahla <73160037+YaYa-Ntlahla@users.noreply.github.com>
2024-05-07 22:42:23 -0700

    The Scanner class is used to get user input, and it is found in the java.util package

---
+++ IntergerSumExample.java
@@ -0,0 +1,25 @@
+
+
+
+
+
+import java.util.Scanner;
+public class IntergerSumExample {
+    public static void main(String[] args){
+    Scanner readInput = new Scanner(System.in);
+    System.out.print("First Number:");
+    float a =readInput.nextFloat();
+    System.out.print("Second Number.");
+     float b= readInput.nextFloat();
+     readInput.close();
+
+     // It will return the sum of a and b.
+        System.out.print("The sum of a and b is =" +float.sum(a,b));
+
+
+
+    }
+
+
+
+}
